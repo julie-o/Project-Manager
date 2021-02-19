e884d0507cbe18599e8454c068de4faf79001890
Julie <23364336+julie-o@users.noreply.github.com>
2021-02-19 14:30:29 +0000

    Added functionality for starting and exiting program

--- src/project_manager.c
+++ src/project_manager.c
@@ -1,7 +1,50 @@
+/***
+ * 
+ */
+
 #include <stdio.h>
+#include <stdbool.h>
+#include "project.c"
+
+#define EXIT 0
+
+
+int current_action;
+
+void prompt_action(){
+	printf("Choose action:\n");
+	scanf("%d", &current_action);
+}
+
+void action(int current_action)
+{
+	switch (current_action) {
+		case 0:
+			break;
+		default:
+			// "No such action found"
+			break;
+	}
+}
+
+void start_manager(){
+	printf("Project Manager starting\n");
+}
 
 int main()
 {
-	printf("Hello, World! \n");
-	return 0;
+
+	start_manager();
+
+	while (true) {
+
+		prompt_action();
+		
+		action(current_action);
+
+		if (current_action==EXIT)
+		{
+			break;
+		}
+	}
 }
